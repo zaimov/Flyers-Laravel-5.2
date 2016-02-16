f7380b6709cfa7cf35f239da29054577ac2030a6
zaimov <zaimov@bitbucket.org>
2016-02-16 22:53:03 +0200

    Added Country utility and form to create view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,22 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('pages.home');
+    });
 
-Route::get('/', function () {
-    return view('pages.home');
+    Route::resource('flyers', 'FlyersController');
 });
-
-Route::resource('flyers', 'FlyersController');
-
-/*Route::group(['middleware' => ['web']], function () {
-    //
-});*/
